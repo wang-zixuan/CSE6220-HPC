fca09f61c2c24eead50ae90a5f9fa2d3dd56e9cb
wang-zixuan <zxwang9811@gmail.com>
2024-02-07 09:52:29 -0500

    pa1

---
+++ pa1/README.txt
@@ -0,0 +1,12 @@
+This program calculates the value of pi using the Monte Carlo method with MPI for parallel processing.
+
+Below is the workflow of the program:
+1) The processor with rank 0 takes n as the total number of points used for the estimation. 
+
+2) n points will be distributed across MPI processes, and each process generates pairs of random numbers within the [0, 1] range to simulate points in a unit square, determining if they fall inside a quarter circle.
+
+3) The results are then aggregated to the processor with rank 0 by MPI_reduce to estimate pi.
+
+The machine we used for generating the results:
+
+We ran our code on PACE-ICE. We allocated 64 processes and then tested our algorithm by passing different parameters to srun (see job-sbatch). The output is in slurm-273389.out. PACE-ICE automatically allocated 4 nodes with 64 cpus for us.
---
+++ pa1/main.cpp
@@ -0,0 +1,44 @@
+#include <getopt.h>
+#include "mpi.h"
+#include <iostream>
+
+#define MASTER 0
+
+int main(int argc, char* argv[]) {
+    int rank, size;
+    long long n, local_count = 0, total_count;
+    double pi_estimate = 0.0, start_time, end_time, x, y;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // number of points
+    if (rank == MASTER) {
+        n = (long long) (std::stod(argv[1]));
+        start_time = MPI_Wtime();
+    }
+
+    MPI_Bcast(&n, 1, MPI_LONG_LONG_INT, MASTER, MPI_COMM_WORLD);
+    
+    srand(time(NULL) + rank);
+
+    for (long long i = rank + 1; i <= n; i += size) {
+        x = (double) rand() / RAND_MAX;
+        y = (double) rand() / RAND_MAX;
+        if (x * x + y * y <= 1) {
+            local_count++;
+        }
+    }
+
+    MPI_Reduce(&local_count, &total_count, 1, MPI_LONG_LONG_INT, MPI_SUM, MASTER, MPI_COMM_WORLD);
+
+    if (rank == MASTER) {
+        pi_estimate = (4.0 * total_count) / (double) n;
+        end_time = MPI_Wtime();
+        printf("%.12f, %f\n", pi_estimate, end_time - start_time);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
